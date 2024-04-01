9e2eee607d4f4aa1b81636b856882482a996ef0f
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-01 18:44:04 +0530

    commit

--- src/Components/Random.jsx
+++ src/Components/Random.jsx
@@ -1,8 +1,27 @@
 import React, { useState } from "react";
+import axios from "axios";
+import {useEffect} from 'react';
+
+
+const API_KEY = '1gmM18OiuKPiirUvDHyYXlb9kKj8CkpY';
 
 const Random = () => {
-  const [gif, setGif] = useState("");
-  function clickHandler() {}
+
+  const [gif, setGif] = useState('');
+  
+  async function fetchData(){
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const {data} = await axios.get(url);
+    // console.log(output);
+    const imageSource = data.data.images.downsized_large.url;
+    setGif(imageSource)
+  }
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  function clickHandler() {
+    fetchData();
+  }
 
   return (
     <div className="w-1/2 h-[450px] bg-green-400 rounded-lg border border-white flex flex-col gap-y-5 mt-[15px] items-center ">
