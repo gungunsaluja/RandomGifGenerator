0f23abd6593c7c57c53001f037e82f2033e4fd1f
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-01 20:05:03 +0530

    commit

--- src/Components/Random.jsx
+++ src/Components/Random.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import {useEffect} from 'react';
+import Spinner from "./Spinner";
 
 
 const API_KEY = '1gmM18OiuKPiirUvDHyYXlb9kKj8CkpY';
@@ -8,30 +9,36 @@ const API_KEY = '1gmM18OiuKPiirUvDHyYXlb9kKj8CkpY';
 const Random = () => {
 
   const [gif, setGif] = useState('');
+  const [loading,setLoading] = useState('false');
   
   async function fetchData(){
+    setLoading(true);
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
     const {data} = await axios.get(url);
     // console.log(output);
     const imageSource = data.data.images.downsized_large.url;
-    setGif(imageSource)
+    setGif(imageSource);
+    setLoading(false);
   }
-  useEffect(()=>{
+  useEffect( () => {
     fetchData();
-  },[]);
+  },[] );
   function clickHandler() {
     fetchData();
   }
 
   return (
-    <div className="w-1/2 h-[450px] bg-green-400 rounded-lg border border-white flex flex-col gap-y-5 mt-[15px] items-center ">
-      <h1 className="text-3xl underline uppercase font-extrabold">
+    <div className="w-1/2 bg-green-400 rounded-lg border border-white flex flex-col gap-y-5 mt-[15px] items-center">
+      <h1 className="mt-[15px] text-2xl underline uppercase font-extrabold">
         A Random Gif
       </h1>
-      <img src={gif} width="450" />
+      {
+        loading?(<Spinner/>) :(<img src = {gif} width = "450"/>)
+      }
+      
       <button
         onClick={clickHandler}
-        className="w-10/12 bg-white opacity-60 text-lg py-2 rounded-lg "
+        className="w-10/12 bg-white opacity-60 text-lg py-2 rounded-lg mb-[20px]"
       >
         Generate
       </button>
--- src/Components/Tag.jsx
+++ src/Components/Tag.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import {useEffect} from 'react';
+import Spinner from "./Spinner";
+
+
+const API_KEY = '1gmM18OiuKPiirUvDHyYXlb9kKj8CkpY';
 
 const Tag = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [tag, setTag] = useState('car');
+  const [gif, setGif] = useState('');
+  const [loading,setLoading] = useState('false');
+  
+  async function fetchData(){
+    setLoading(true);
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+    const {data} = await axios.get(url);
+    // console.log(output);
+    const imageSource = data.data.images.downsized_large.url;
+    setGif(imageSource);
+    setLoading(false);
+  }
+  useEffect( () => {
+    fetchData();
+  },[] );
 
-export default Tag
+  function clickHandler() {
+    fetchData();
+  }
+
+  function changeHandler(event){
+
+    setTag(event.target.value);
+  }
+
+  return (
+    <div className="w-1/2 bg-blue-500 rounded-lg border border-white flex flex-col gap-y-5 mt-[15px] items-center">
+      <h1 className="mt-[15px] text-2xl underline uppercase font-extrabold">
+        Random Gif
+      </h1>
+      {
+        loading ? (<Spinner/>) :(<img src = {gif} width = "450"/>)
+      }
+      
+
+      <input className = "w-10/12 text-lg py-2 rounded-lg mb-[20px] text-center " onChange={(event)=>setTag(event.target.value)} value = {tag} />
+      <button
+        onClick={clickHandler}
+        className="w-10/12 bg-white opacity-60 text-lg py-2 rounded-lg mb-[3px] text-center" 
+      >
+        Generate
+      </button>
+    </div>
+  );
+};
+
+export default Tag;
